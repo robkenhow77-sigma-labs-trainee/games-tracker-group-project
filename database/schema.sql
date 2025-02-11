19667cd067dfa2e7b27ad62e63c51ae3c1227bad
Abdirahman Mohamud <127400865+OfficialARM17@users.noreply.github.com>
2025-02-11 14:04:38 +0000

    Update schema.sql
    Changed the order of the ALTER TABLE statements to match up together and also made Age rating nullable since for some reason games on Steam that are made by Valve do not have a rating

--- database/schema.sql
+++ database/schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE "game"(
     "game_title" VARCHAR(100) NOT NULL,
     "release_date" DATE NOT NULL,
     "game_image" VARCHAR(255) NOT NULL,
-    "age_rating" SMALLINT NOT NULL
+    "age_rating" SMALLINT NULL
 );
 
 
@@ -88,13 +88,26 @@ CREATE TABLE "publisher_game_assignment"(
 
 -- Adding all of the constraints for each table
 
+-- Developer Game Assignment
 ALTER TABLE "developer_game_assignment" 
     ADD CONSTRAINT "developer_game_assignment_game_id_foreign" 
     FOREIGN KEY("game_id") REFERENCES "game"("game_id");
 
+ALTER TABLE "developer_game_assignment" 
+    ADD CONSTRAINT "developer_game_assignment_developer_id_foreign" 
+    FOREIGN KEY("developer_id") REFERENCES "developer"("developer_id");
+
+-- Tag Game Platform Assignment
+
 ALTER TABLE "tag_game_platform_assignment" 
     ADD CONSTRAINT "tag_game_platform_assignment_platform_assignment_id_foreign" 
     FOREIGN KEY("platform_assignment_id") REFERENCES "game_platform_assignment"("platform_assignment_id");
+
+ALTER TABLE "tag_game_platform_assignment" 
+    ADD CONSTRAINT "tag_game_platform_assignment_tag_id_foreign" 
+    FOREIGN KEY("tag_id") REFERENCES "tag"("tag_id");
+
+-- Genre Game Platform Assignment
 
 ALTER TABLE "genre_game_platform_assignment" 
     ADD CONSTRAINT "genre_game_platform_assignment_genre_id_foreign" 
@@ -104,21 +117,17 @@ ALTER TABLE "genre_game_platform_assignment"
     ADD CONSTRAINT "genre_game_platform_assignment_platform_assignment_id_foreign" 
     FOREIGN KEY("platform_assignment_id") REFERENCES "game_platform_assignment"("platform_assignment_id");
 
-ALTER TABLE "tag_game_platform_assignment" 
-    ADD CONSTRAINT "tag_game_platform_assignment_tag_id_foreign" 
-    FOREIGN KEY("tag_id") REFERENCES "tag"("tag_id");
+-- Game Platform Assignment
 
 ALTER TABLE "game_platform_assignment" 
     ADD CONSTRAINT "game_platform_assignment_platform_id_foreign" 
     FOREIGN KEY("platform_id") REFERENCES "platform"("platform_id");
 
-ALTER TABLE "developer_game_assignment" 
-    ADD CONSTRAINT "developer_game_assignment_developer_id_foreign" 
-    FOREIGN KEY("developer_id") REFERENCES "developer"("developer_id");
-
 ALTER TABLE "game_platform_assignment" 
     ADD CONSTRAINT "game_platform_assignment_game_id_foreign" 
     FOREIGN KEY("game_id") REFERENCES "game"("game_id");
+
+-- Publisher Game Assignment
 
 ALTER TABLE "publisher_game_assignment" 
     ADD CONSTRAINT "publisher_game_assignment_game_id_foreign" 
