80bb860149962d2ab1a2ea23cf27b93b8157d3e2
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-12 18:24:44 +0000

    loading

--- pipeline/load_functions.py
+++ pipeline/load_functions.py
@@ -51,7 +51,7 @@ def get_genre_ids(conn: psycopg.Connection):
         return cur.fetchall()
 
 
-def execute_and_return_devs(devs: list[tuple], conn: psycopg.Connection):
+def upload_and_return_devs(devs: list[tuple], conn: psycopg.Connection):
     """Loads values and returns"""
     try:
         with conn.cursor() as cur:
@@ -68,7 +68,7 @@ def execute_and_return_devs(devs: list[tuple], conn: psycopg.Connection):
         return {}
 
 
-def execute_and_return_games(games: list[tuple], conn: psycopg.Connection):
+def upload_and_return_games(games: list[tuple], conn: psycopg.Connection):
     """Loads values and returns"""
     try:
         with conn.cursor() as cur:
@@ -85,7 +85,7 @@ def execute_and_return_games(games: list[tuple], conn: psycopg.Connection):
         return {}
 
 
-def execute_and_return_pubs(pubs: list[tuple], conn: psycopg.Connection):
+def upload_and_return_pubs(pubs: list[tuple], conn: psycopg.Connection):
     """Loads values and returns"""
     try:
         with conn.cursor() as cur:
@@ -102,7 +102,7 @@ def execute_and_return_pubs(pubs: list[tuple], conn: psycopg.Connection):
         return {}
 
 
-def execute_and_return_genres(genre: list[tuple], conn: psycopg.Connection):
+def upload_and_return_genres(genre: list[tuple], conn: psycopg.Connection):
     """Loads values and returns"""
     try:
         with conn.cursor() as cur:
@@ -119,7 +119,7 @@ def execute_and_return_genres(genre: list[tuple], conn: psycopg.Connection):
         return {}
 
 
-def execute_and_return_tags(tags: list[tuple], conn: psycopg.Connection):
+def upload_and_return_tags(tags: list[tuple], conn: psycopg.Connection):
     """Loads values and returns"""
     try:
         with conn.cursor() as cur:
@@ -138,7 +138,7 @@ def execute_and_return_tags(tags: list[tuple], conn: psycopg.Connection):
 
 def get_game_platform_assignments(conn: psycopg.Connection):
     sql = """
-    SELECT game_id, platform_id
+    SELECT platform_assignment_id, game_id, platform_id
     FROM game_platform_assignment
     """
     with conn.cursor() as cur:
@@ -146,7 +146,7 @@ def get_game_platform_assignments(conn: psycopg.Connection):
         return cur.fetchall()
 
 
-def assign_publishers(new_games_list: list[dict], game_id_mapping: dict, publisher_mapping):
+def assign_publishers(new_games_list: list[dict], game_id_mapping: dict, publisher_mapping, current: list[tuple]):
     """Maps the names to the ids and the pubs to ids, for the assignment table"""
     values = []
     for game in new_games_list:
@@ -162,10 +162,10 @@ def assign_publishers(new_games_list: list[dict], game_id_mapping: dict, publish
                 game_id_mapping[game["game_name"]],
                 publisher_mapping[game["publisher"]]
             ))
-    return values
+    return [value for value in values if value not in current]
 
 
-def assign_developers(new_games_list: list[dict], game_id_mapping: dict, developer_mapping):
+def assign_developers(new_games_list: list[dict], game_id_mapping: dict, developer_mapping, current: list[tuple]):
     """Maps the names to the ids and the pubs to ids, for the assignment table"""
     values = []
     for game in new_games_list:
@@ -181,7 +181,7 @@ def assign_developers(new_games_list: list[dict], game_id_mapping: dict, develop
                 game_id_mapping[game["game_name"]],
                 developer_mapping[game["developer"]]
             ))
-    return values
+    return [value for value in values if value not in current]
 
 
 def upload_developer_assignment(data: list[tuple], conn: psycopg.Connection):
@@ -196,3 +196,68 @@ def upload_publisher_assignment(data: list[tuple], conn: psycopg.Connection):
         cur.executemany("""INSERT INTO publisher_game_assignment (game_id, publisher_id) 
             VALUES (%s, %s)""", data)
         conn.commit()
+
+
+def get_publisher_game_assignments(conn: psycopg.Connection):
+    sql = """
+    SELECT *
+    FROM publisher_game_assignment
+    """
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        return cur.fetchall()
+    
+
+def get_developer_game_assignments(conn: psycopg.Connection):
+    sql = """
+    SELECT *
+    FROM developer_game_assignment
+    """
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        return cur.fetchall()
+
+
+def get_platform_mapping(conn: psycopg.Connection):
+    sql = """
+    SELECT *
+    FROM platform
+    """
+    with conn.cursor() as cur:
+        cur.execute(sql)
+        return cur.fetchall()
+
+
+def assign_game_platform(new_games_list: list[dict], game_id_mapping: dict, platform_mapping: dict, current: list[tuple]):
+    """Maps the names to the ids and the pubs to ids, for the assignment table"""
+    values = []
+    for game in new_games_list:
+        new_assignment_tuple = (
+            game_id_mapping[game["game_name"]], platform_mapping[game["platform"]]
+        )
+        if new_assignment_tuple not in current:
+            values.append((
+                game_id_mapping[game["game_name"]], # game_id
+                platform_mapping[game["platform"]], # platform_id
+                game["score"], # platform_score
+                game["price"], # platform_price
+                game["discount"] # platform_discount
+            ))
+    return values
+
+
+def upload_and_return_game_platform_assignment(data: list[tuple], conn: psycopg.Connection):
+    """Loads values and returns"""
+    try:
+        with conn.cursor() as cur:
+            cur.executemany("""INSERT INTO game_platform_assignment (game_id, platform_id, platform_score, platform_price, platform_discount) 
+            VALUES (%s, %s, %s, %s, %s) RETURNING platform_assignment_id, game_id""", data, returning=True)
+            ids = []
+            while True:
+                ids.append(cur.fetchone())
+                if not cur.nextset():
+                    break
+            conn.commit()
+            return ids
+    except :
+        return {}
