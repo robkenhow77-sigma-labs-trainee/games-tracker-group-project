fbc794c2e4a942051cee76d0acc19defd610c0a4
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-12 11:26:28 +0000

    load

---
+++ pipeline/test_seeding.sql
@@ -0,0 +1,8 @@
+INSERT INTO developer (developer_name)
+VALUES ('treyarch');
+
+INSERT INTO publisher (publisher_name)
+VALUES ('ubisoft');
+
+INSERT INTO game (game_title, release_date, game_image, age_rating)
+VALUES ('Black ops', NOW(), 'url', 18);
