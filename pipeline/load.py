c4d2a9fd2543593e84456de21695c936a324fb41
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-02-12 12:16:35 +0000

    loading

--- pipeline/load.py
+++ pipeline/load.py
@@ -17,7 +17,7 @@ def get_ids(table_name: str, conn: psycopg.Connection):
     query = f"""
         SELECT * FROM {table_name};
     """
-    with conn.cursor(row_factory=dict_row) as cur:
+    with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
@@ -27,25 +27,26 @@ def make_id_mapping(ids_and_items: list[dict], item: str):
     return {id_and_item[f'{item}_name']: id_and_item[f'{item}_id'] for id_and_item in ids_and_items}
 
 
+def get_new_items_set(item: str, games_list_dict: list[dict]) -> list[str]:
+    """Gets the specified item from each dictionary
+    Eg. get all the game titles
+    """
+    items = set()
+    for game in games_list_dict:
+        value = game[item]
+        if isinstance(value, str):
+            items.add(value)
+        else:
+            for val in value:
+                items.add(val)
+    return items
+
+
 def get_items_not_in_current(new: list[str], current: list[str]):
     """Checks the new list to find any strings not in the current list. 
     Eg. new tags that aren't already in the database"""
     return [word for word in new if word not in current]
 
-
-def get_new_items_list(item: str, games_list_dict: list[dict]) -> list[str]:
-    """Gets the specified item from each dictionary
-    Eg. get all the game titles
-    """
-    items = []
-    for game in games_list_dict:
-        value = game[item]
-        if isinstance(value, str):
-            items.append(value)
-        else:
-            for val in value:
-                items.append(val)
-    return items
 
 
 # Get devs, pubs, tags, genres and games for upload
@@ -53,13 +54,13 @@ def get_items_for_upload(table: str, new_games: list[dict], conn: psycopg.Connec
     """Gets all the new genres for uploading to the database"""
     ids = get_ids(table, conn)
     current = make_id_mapping(ids, table)
-    new = get_new_items_list(table, new_games)
+    new = get_new_items_set(table, new_games)
     return  get_items_not_in_current(new, current.keys())
 
 
 # Dev_assignment, pub_assignment
-# Need game id and corresponding pub/dev ids
-def get_new_game_ids(table: str, new_games: list[dict], conn: psycopg.Connection):
+def get_new_item_ids(table: str, new_games: list[dict], conn: psycopg.Connection):
+    """Gets the ids that the items will have when they get uploaded to the database"""
     items_for_upload = get_items_for_upload(table, new_games, conn)
     ids = get_ids(table, conn)
     max_id = max([game[f"{table}_id"] for game in ids])
@@ -80,27 +81,39 @@ def pub_or_dev_game_assignment(game_ids: dict, pub_or_dev_ids: dict) -> list[tup
     return assignments
 
 
+# Game_platform_assignment
+# Check if the game exists on all 3 platforms
+def get_game_assignments(conn: psycopg.Connection):
+    """Gets the game assignment table"""
+    query = """
+        SELECT game_id, COUNT(platform_id) 
+        FROM game_platform_assignment
+        GROUP BY game_id;
+    """
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
 
 if __name__ == "__main__":
     load_dotenv()
     conn_string = f"postgresql://{ENV['DB_USERNAME']}:{ENV["DB_PASSWORD"]}@{ENV["DB_HOST"]}:{ENV["DB_PORT"]}/{ENV["DB_NAME"]}"
-    connection = psycopg.connect(conn_string)
-    
+    connection = psycopg.connect(conn_string, row_factory=dict_row)
+
+
     new_games_example = [{
         "game": "fortnite",
-        "developer": ["treyarch", 'epic', 'goat'],
+        "developer": ["treyarch", 'epic', 'some other dev'],
         "tag": ["action"],
         "genre": "mystic",
         "publisher": "sigma"
         }]
 
-    get_new_game_ids()
 
-
-    new_game_ids = get_new_game_ids('game', new_games_example, connection)
-    print(new_game_ids)
-
-    # print(pub_or_dev_game_assignment(new_game_ids, new_developer_ids))
-
-
+    new_game_ids = get_new_item_ids('game', new_games_example, connection)
+    current_game_ids = make_id_mapping(get_ids('game', connection))
+    print(current_game_ids)
+    
+  
+  
     connection.close()
--- pipeline/test_seeding.sql
+++ pipeline/test_seeding.sql
@@ -6,3 +6,6 @@ VALUES ('ubisoft');
 
 INSERT INTO game (game_title, release_date, game_image, age_rating)
 VALUES ('Black ops', NOW(), 'url', 18);
+
+INSERT INTO game_platform_assignment (game_id, platform_id, platform_score, platform_price, platform_discount)
+VALUES (2, 2, 10, 10, 0);
