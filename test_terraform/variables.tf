4235b03409aec0973ab92bb960f7848034f45eb0
Abdirahman Mohamud <officialarm100@gmail.com>
2025-02-14 15:25:18 +0000

    Made a test folder for making Terraform resources and remade the schema based on some work

---
+++ test_terraform/variables.tf
@@ -0,0 +1,42 @@
+# Define variables used in the config
+
+variable "AWS_ACCESS_KEY" {
+    type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+    type = string
+}
+
+variable "AWS_REGION" {
+    type = string
+    default = "eu-west-2"
+}
+
+variable "VPC_ID" {
+    type = string
+}
+
+variable "DB_HOST" {
+    type = string  
+}
+
+variable "DB_PORT" {
+    type = string
+}
+
+variable "DB_USERNAME" {
+    type = string  
+}
+
+variable "DB_PASSWORD" {
+    type = string
+}
+
+variable "DB_NAME" {
+    type = string  
+}
+
+variable "ABDI_EMAIL" {
+    type = string
+}
