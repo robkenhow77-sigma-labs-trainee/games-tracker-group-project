dfe916d85e6197a9dc7433e32a9707dcd501e01a
jadg02 <jgroom122@gmail.com>
2025-02-14 11:43:30 +0000

    lint subscribe file

--- dashboard/pages/subscribe.py
+++ dashboard/pages/subscribe.py
@@ -1,7 +1,9 @@
 """dashboard page allowing users to subscribe to newsletters"""
+# pylint: disable=line-too-long
+# pylint: disable=broad-exception-caught
+from os import environ as ENV
 import streamlit as st
 import boto3
-from os import environ as ENV
 from dotenv import load_dotenv
 from components import dashboard_title
 
@@ -9,9 +11,9 @@ from components import dashboard_title
 def sns_connect():
     """connect to sns client"""
     client = boto3.client(
-    'sns',
-    aws_access_key_id=ENV['AWS_ACCESS_KEY'],
-    aws_secret_access_key=ENV['AWS_SECRET_ACCESS_KEY']
+        'sns',
+        aws_access_key_id=ENV['AWS_ACCESS_KEY'],
+        aws_secret_access_key=ENV['AWS_SECRET_ACCESS_KEY']
     )
     return client
 
@@ -24,38 +26,39 @@ def get_or_create_topic(client, genre):
         if f'play_stream_{genre}' in topic["TopicArn"]:
             print(f"Topic '{genre}' already exists.")
             return topic["TopicArn"]
-    
-    res = client.create_topic(Name = f'play_stream_{genre}')
+
+    res = client.create_topic(Name=f'play_stream_{genre}')
     print('Created new topic ', genre)
-    
+
     return res['TopicArn']
 
 
 def is_already_subscribed(client, email, topic_arn):
     """checks if a user's email is already subscribed to the given genre/weekly digest"""
-    existing_subs = client.list_subscriptions_by_topic(TopicArn=topic_arn).get('Subscriptions',[])
+    existing_subs = client.list_subscriptions_by_topic(
+        TopicArn=topic_arn).get('Subscriptions', [])
     print('existing subs: ', existing_subs)
 
     for sub in existing_subs:
         if sub['Protocol'] == 'email' and sub['Endpoint'] == email and sub['SubscriptionArn'] != 'PendingConfirmation' and sub['SubscriptionArn'] != 'Deleted':
-            return True 
+            return True
         return False
-            
+
 
 def subscribe_user(email, genre, weekly_digest):
     """subscribes the user to sns emailing list for genres and weekly digest"""
 
     if not email:
         st.error("Email is required!")
-    
+
     if not genre and not weekly_digest:
         st.error("A genre or weekly digest needs to be included")
 
     client = sns_connect()
 
-    if email and genre: #if inputted a genre and email do this
+    if email and genre:  # if inputted a genre and email do this
         topic_arn = get_or_create_topic(client, genre)
-        if is_already_subscribed(client,email,topic_arn):
+        if is_already_subscribed(client, email, topic_arn):
             st.info('This email is already subscribed to the following genres')
         else:
             try:
@@ -71,16 +74,17 @@ def subscribe_user(email, genre, weekly_digest):
 
     if email and weekly_digest:
         digest_topic_arn = get_or_create_topic(client, 'weekly_digest')
-        if is_already_subscribed(client,email,digest_topic_arn):
+        if is_already_subscribed(client, email, digest_topic_arn):
             st.info('This email is already subscribed to the weekly digest')
         else:
             try:
                 client.subscribe(
                     TopicArn=digest_topic_arn,
                     Protocol="email",
-                    Endpoint=email 
+                    Endpoint=email
                 )
-                st.success(f'Your email has been subscribed to the weekly digest')
+                st.success(
+                    'Your email has been subscribed to the weekly digest')
                 print(f'Subscribed {email} to newsletter')
             except Exception as e:
                 st.error(f'Subscription to newsletter failed: {e}')
@@ -93,7 +97,9 @@ if __name__ == "__main__":
     st.html("<marquee>Welcome to MySpace!</marquee>")
     f_name = st.text_input("First Name")
     l_name = st.text_input("Last Name")
-    email = st.text_input("Email")
-    genre = st.text_input("Genre")
-    weekly_digest = st.checkbox("Subscribe to Weekly Digest", value=True)
-    st.button("Submit", on_click=lambda: subscribe_user(email, genre.lower(), weekly_digest))
+    user_email = st.text_input("Email")
+    selected_genre = st.text_input("Genre")
+    accepted_weekly_digest = st.checkbox(
+        "Subscribe to Weekly Digest", value=True)
+    st.button("Submit", on_click=lambda: subscribe_user(
+        user_email, selected_genre.lower(), accepted_weekly_digest))
