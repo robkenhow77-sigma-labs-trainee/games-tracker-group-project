6a16280d2ec88f5a29a4a2b7f4a2f0e42b62fceb
Candice-Bennett <Candice.Bennett123@gmail.com>
2025-02-19 15:58:19 +0000

    updated scripts

--- email_lambdas/weekly-digest/weekly_digest_ECR.sh
+++ email_lambdas/weekly-digest/weekly_digest_ECR.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
-docker build -t c15-play-stream-weekly-email-summary . --platform="linux/amd64" --provenance=False
-docker tag c15-play-stream-weekly-email-summary:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-play-stream-weekly-email-summary:latest
-docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-play-stream-weekly-email-summary:latest
+docker build -t c15-play-steam-weekly-email . --platform="linux/amd64" --provenance=False
+docker tag c15-play-steam-weekly-email:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-play-steam-weekly-email:latest
+docker push 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c15-play-steam-weekly-email:latest
