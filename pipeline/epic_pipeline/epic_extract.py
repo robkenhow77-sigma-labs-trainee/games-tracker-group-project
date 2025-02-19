4cd3b6ff217d1d2abfd2e15974bc15fd2f78d4eb
CodeTechBen <benjamin-smith@live.com>
2025-02-19 11:21:57 +0000

    gets link

--- pipeline/epic_pipeline/epic_extract.py
+++ pipeline/epic_pipeline/epic_extract.py
@@ -74,6 +74,12 @@ def get_pegi_age_control(game: dict) -> str:
 
     return None
 
+def get_link(game: dict) -> str:
+    mappings = game.get("catalogNs", {}).get("mappings")
+    link = 'https://store.epicgames.com/en-US/p/' + mappings[0].get('pageSlug')
+    print(link)
+    return link if link else None
+
 
 def format_data(games: list[dict]) -> list[dict]:
     """Formats raw game data into a standardized list of dictionaries."""
@@ -93,7 +99,8 @@ def format_data(games: list[dict]) -> list[dict]:
             "platform_discount": game.get("price", {}).get("totalPrice", {}).get("discountPercentage"),
             "release_date": game.get("releaseDate"),
             "game_image": game.get("keyImages", [{}])[0].get("url"),
-            "age_rating": get_pegi_age_control(game)
+            "age_rating": get_pegi_age_control(game),
+            "link": get_link(game)
         }
         game_list.append(game_data)
 
