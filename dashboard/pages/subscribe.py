e2a68365f5e88772341046c601605fc2f899c1a8
jadg02 <jgroom122@gmail.com>
2025-02-20 12:45:18 +0000

    migrate data source code to subscribe page

--- dashboard/pages/subscribe.py
+++ dashboard/pages/subscribe.py
@@ -5,7 +5,33 @@ from os import environ as ENV
 import streamlit as st
 import boto3
 from dotenv import load_dotenv
-from data_source import get_connection, get_all_genres
+import pandas as pd
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+
+@st.cache_resource
+def get_connection():
+    """connects to the games database"""
+    connection = psycopg2.connect(
+        dbname=ENV['DB_NAME'],
+        user=ENV['DB_USERNAME'],
+        password=ENV['DB_PASSWORD'],
+        host=ENV['DB_HOST'],
+        port=ENV['DB_PORT'],
+        cursor_factory=RealDictCursor)
+
+    return connection
+
+
+@st.cache_data
+def get_all_genres(_conn):
+    """gets all game genres listed in the database"""
+    query = "select genre_name from genre"
+    with _conn.cursor() as cur:
+        cur.execute(query)
+        res = cur.fetchall()
+    return pd.DataFrame(res)
 
 
 def sns_connect():
